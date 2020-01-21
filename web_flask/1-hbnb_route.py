8cf04dd889bc3881363b84c5ea9ce43e1aac611b
arq-gabo <jgabrielguerral@gmail.com>
2020-01-21 00:02:29 +0000

    [FIX]Task_01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,15 +9,9 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
-@app.route('/')
-def hello():
-    return('Hola HBNB!')
-
 @app.route('/hbnb')
 def hbnb():
     return('HBNB')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
