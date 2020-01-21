e4257fb391744486cbdf6eae1662fe1e1530d70d
arq-gabo <jgabrielguerral@gmail.com>
2020-01-21 21:33:29 +0000

    [FEATURE]Task_04

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+Module for start Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Show Hello HBNB! in the index page"""
+    return('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Show HBNB if the argument of parameter url is /hbnb"""
+    return('HBNB')
+
+
+@app.route('/c/<text>')
+def cis(text):
+    """Show the argument of url"""
+    return('C {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    """Show python Following the value of the text value"""
+    return("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """Show number the value of the number in the URL"""
+    return("{} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
