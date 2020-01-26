dcd1c3c145ce54abadc8c08bd89f93dc3f7437fe
arq-gabo <jgabrielguerral@gmail.com>
2020-01-25 21:36:55 -0500

    [FIX]Task_08

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,23 +8,21 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
-def displayStates():
+def display():
     """Show States"""
     all_states = storage.all("State")
-    return(render_template('7-states_list.html', State=all_states))
+    return render_template('7-states_list.html', State=all_states)
 
 
 @app.teardown_appcontext
-def close(exception=None):
+def close_method(exception=None):
     """Close Method"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000)
